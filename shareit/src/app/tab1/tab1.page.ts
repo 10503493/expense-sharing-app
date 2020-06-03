360e1ee230dcd2cacfce97a3a8e68928771c6da4
Tony <56820131+10503493@users.noreply.github.com>
2020-06-03 21:12:09 +0100

    CategoryFinder,Group,AddExpense

--- shareit/src/app/tab1/tab1.page.ts
+++ shareit/src/app/tab1/tab1.page.ts
@@ -5,16 +5,24 @@ import { Component } from '@angular/core';
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page 
-{
 
-  constructor() {}
-cli()
-  {
-alert('jkgg')
+export class Tab1Page {
+  constructor() { }
+
+
+  GroupSelected(group) {
+    alert(group)
   }
-  
+  CategoryFinder(Category) {
+    alert(Category);
+    let categoryvalue=Category;
+  }
+  AddExpense()
+  {
+    let expense=5;
+    console.log(expense , )
+  }
+
 }
 
 
-  
