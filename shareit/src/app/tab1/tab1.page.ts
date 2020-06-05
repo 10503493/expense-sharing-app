802bfae4adf35abcf2ca5415e80b5dca412cd18f
Tony <56820131+10503493@users.noreply.github.com>
2020-06-05 15:26:15 +0100

    total expense-switch

--- shareit/src/app/tab1/tab1.page.ts
+++ shareit/src/app/tab1/tab1.page.ts
@@ -9,8 +9,24 @@ import { $ } from 'protractor';
 
 export class Tab1Page {
   Category: any;
-  total=0;
-  food=0;
+  total = 0;
+  education = 0;
+  grocery = 0;
+  food = 0;
+  drinks = 0;
+  restaurant = 0;
+  tips = 0;
+  health = 0;
+  cloths = 0;
+  fun = 0;
+  personal = 0;
+  merchandise = 0;
+  rent = 0;
+  pets = 0;
+  transportation = 0;
+  fuel = 0;
+  others = 0;
+
   constructor() { }
 
 
@@ -18,19 +34,111 @@ export class Tab1Page {
     alert(group)
   }
   CategoryFinder(Category) {
-    alert(Category);
-   
+    //alert(Category);
+
   }
 
-  AddExpense(Category,expense)
-  {
-    if (Category=='Food')
+  AddExpense(Category, expense)
+   {
+    switch (Category) 
     {
-      this.food+=expense;
+      case 'Education':
+        {
+          this.education += expense;
+          //console.log(this.education,Category)
+          break;
+        }
+
+      case 'Grocery':
+        {
+          this.grocery += expense;
+          //console.log(this.grocery,Category)
+          break;
+        }
+      case 'Food':
+        {
+        this.food += expense;
+        break;
+        }
+      case 'Drinks':
+        {
+        this.drinks += expense;
+        break;
+        }
+      case 'Restaurant':
+        {
+        this.restaurant += expense;
+        break;
+        }
+        case 'Tips':
+        {
+        this.tips += expense;
+        break;
+        }
+        case 'Health':
+        {
+        this.health += expense;
+        break;
+        }
+        case 'Cloths':
+        {
+        this.cloths += expense;
+        break;
+        }
+        case 'Fun':
+        {
+        this.fun += expense;
+        break;
+        }
+        case 'Drinks':
+        {
+        this.drinks += expense;
+        break;
+        }
+        case 'Personal':
+        {
+        this.personal += expense;
+        break;
+        }
+        case 'Merchandise':
+        {
+        this.merchandise += expense;
+        break;
+        }
+        case 'Rent':
+        {
+        this.rent += expense;
+        break;
+        }
+        case 'Pets':
+        {
+        this.pets += expense;
+        break;
+        }
+        case 'Transportation':
+        {
+        this.transportation += expense;
+        break;
+        }
+        case 'Fuel':
+        {
+        this.fuel += expense;
+        break;
+        }
+        case 'Others':
+        {
+        this.others += expense;
+        break;
+        }
     }
-    this.total=this.total+expense
-    console.log("your toatal expense is ",this.food,"for",Category)
-    
+    //this.total=this.total+expense
+    console.log("your expenses are recorded")
   }
+
+ViewExpense()
+{
+  
+}
+
 
 }
